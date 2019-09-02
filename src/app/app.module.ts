e4d3a49de2ac141f366970e606840b15cea3698a
Rafał Trzop <rafaltrzop@gmail.com>
2019-09-02 11:18:10 +0200

    Add @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { AuthModule } from '@app/auth';
 import { AppRoutingModule } from '@app/app-routing.module';
@@ -9,6 +10,8 @@ import { CoreModule } from '@app/core';
 import { AppComponent } from '@app/core/containers';
 import { FooComponent } from '@app/foo.component'; // TODO: remove
 
+import { reducers, metaReducers } from '@app/reducers';
+
 @NgModule({
   declarations: [FooComponent],
   imports: [
@@ -16,6 +19,13 @@ import { FooComponent } from '@app/foo.component'; // TODO: remove
     BrowserAnimationsModule,
     AuthModule,
     AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
     CoreModule
   ],
   bootstrap: [AppComponent]
