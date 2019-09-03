f8a90995b479c3d6958f9e12a4b83b76e5f0da5f
Rafał Trzop <rafaltrzop@gmail.com>
2019-09-03 10:20:57 +0200

    Refactor main reducers file

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,12 +1,8 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../environments/environment';
+import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
+import { environment } from '@env/environment';
+
+// tslint:disable-next-line:no-empty-interface
 export interface State {}
 
 export const reducers: ActionReducerMap<State> = {};
