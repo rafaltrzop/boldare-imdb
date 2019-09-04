0d331701c400c99aedf91fd450bcfa33dc22f0c5
Rafał Trzop <rafaltrzop@gmail.com>
2019-09-04 22:38:20 +0200

    Display movie ID on movie details page

--- src/app/movies/containers/movie-details-page/movie-details-page.component.ts
+++ src/app/movies/containers/movie-details-page/movie-details-page.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details-page',
   templateUrl: './movie-details-page.component.html',
   styleUrls: ['./movie-details-page.component.scss']
 })
-export class MovieDetailsPageComponent {}
+export class MovieDetailsPageComponent implements OnInit {
+  movieId: string;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.movieId = this.route.snapshot.paramMap.get('id');
+  }
+}
