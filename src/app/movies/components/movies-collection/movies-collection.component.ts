5a3021126636819b175ffa5ba57f3ae230e0120b
Rafał Trzop <rafaltrzop@gmail.com>
2019-09-15 18:36:48 +0200

    Fetch correct data when user navigates using previous or next buttons in the browser

--- src/app/movies/components/movies-collection/movies-collection.component.ts
+++ src/app/movies/components/movies-collection/movies-collection.component.ts
@@ -1,14 +1,18 @@
 import {
   AfterViewInit,
+  ChangeDetectorRef,
   Component,
-  Input,
-  OnInit,
   ViewChild
 } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { MatPaginator, MatSort, SortDirection } from '@angular/material';
+import {
+  MatPaginator,
+  MatSort,
+  MatSortHeader,
+  SortDirection
+} from '@angular/material';
 import { merge, of } from 'rxjs';
-import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { Movie } from '@app/movies/models';
 import { MoviesService } from '@app/movies/services';
@@ -18,10 +22,7 @@ import { MoviesService } from '@app/movies/services';
   templateUrl: './movies-collection.component.html',
   styleUrls: ['./movies-collection.component.scss']
 })
-export class MoviesCollectionComponent implements AfterViewInit, OnInit {
-  // @Input()
-  // movies: Movie[];
-
+export class MoviesCollectionComponent implements AfterViewInit {
   @ViewChild(MatPaginator, { static: false })
   paginator: MatPaginator;
 
@@ -31,8 +32,8 @@ export class MoviesCollectionComponent implements AfterViewInit, OnInit {
   movies: Movie[];
   displayedColumns: string[] = ['position', 'title', 'year', 'metascore'];
   pageSizeOptions: number[] = [5, 10, 25, 100];
-  pageSize: number;
-  pageIndex: number;
+  pageSize = 10;
+  pageIndex = 0;
   sortActive: string;
   sortDirection: SortDirection;
   resultsLength = 0;
@@ -41,46 +42,68 @@ export class MoviesCollectionComponent implements AfterViewInit, OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private ref: ChangeDetectorRef,
     private moviesService: MoviesService
   ) {}
 
-  ngOnInit(): void {
-    const pageSize = +this.route.snapshot.queryParamMap.get('limit');
-    const pageIndex = +this.route.snapshot.queryParamMap.get('page') - 1;
-    const sortActive = this.route.snapshot.queryParamMap.get('sortBy');
-    let sortDirection = this.route.snapshot.queryParamMap.get('sortDir');
-    sortDirection = ['asc', 'desc'].includes(sortDirection)
-      ? sortDirection
-      : 'asc';
-
-    this.pageSize = pageSize || 10;
-    this.pageIndex = pageIndex || 0;
-    this.sortActive = sortActive || 'title';
-    this.sortDirection = sortDirection as SortDirection;
-  }
-
   ngAfterViewInit(): void {
-    // If the user changes the sort order, reset back to the first page.
+    // Go back to the first page on sort order change
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
 
-    merge(this.sort.sortChange, this.paginator.page)
+    // Update URL query string on sort or paginator change
+    merge(this.sort.sortChange, this.paginator.page).subscribe(() => {
+      const limit = this.paginator.pageSize;
+      const page = this.paginator.pageIndex + 1;
+      const sortBy = this.sort.active;
+      const sortDir = this.sort.direction;
+
+      const queryParams: Params = {
+        limit,
+        page,
+        sortBy: sortBy && sortDir ? sortBy : null,
+        sortDir: sortBy && sortDir ? sortDir : null
+      };
+      this.router.navigate([], { queryParams });
+    });
+
+    // Fetch new data on URL query string change
+    this.route.queryParamMap
       .pipe(
-        startWith({}),
-        switchMap(() => {
+        switchMap(queryParamMap => {
           this.isLoadingResults = true;
 
-          const limit = this.paginator.pageSize;
-          const page = this.paginator.pageIndex + 1;
-          const sortBy = this.sort.active;
-          const sortDir = this.sort.direction;
+          let limit = Math.round(Math.abs(+queryParamMap.get('limit')));
+          limit = limit ? limit : this.pageSize;
 
-          const queryParams: Params = {
-            limit,
-            page,
-            sortBy: sortBy && sortDir ? sortBy : null,
-            sortDir: sortBy && sortDir ? sortDir : null
-          };
-          this.router.navigate([], { queryParams });
+          let page = Math.round(Math.abs(+queryParamMap.get('page')));
+          page = page ? page : this.pageIndex + 1;
+
+          let sortBy = queryParamMap.get('sortBy');
+          sortBy = ['title', 'year', 'metascore'].includes(sortBy)
+            ? sortBy
+            : null;
+
+          let sortDir = queryParamMap.get('sortDir') as SortDirection;
+          sortDir = ['asc', 'desc'].includes(sortDir) ? sortDir : null;
+
+          this.paginator.pageSize = limit;
+          this.paginator.pageIndex = page - 1;
+          this.sortActive = sortBy;
+          this.sortDirection = sortDir;
+
+          // Hack for updating Angular Material Sort Header UI
+          // https://github.com/angular/components/issues/10242
+          // https://github.com/angular/components/issues/12754
+          const activeSortHeader = this.sort.sortables.get(
+            sortBy
+          ) as MatSortHeader;
+          if (activeSortHeader) {
+            activeSortHeader._setAnimationTransitionState({
+              fromState: sortDir,
+              toState: 'active'
+            });
+          }
+          this.ref.detectChanges();
 
           return this.moviesService.getMovies({
             limit,
