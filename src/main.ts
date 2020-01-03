e74099b1eb3b366a8454b747fd3c145e8306b4ff
Rafał Trzop <rafaltrzop@gmail.com>
2020-01-03 13:03:42 +0100

    Shorten import path

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app';
-import { environment } from './environments/environment';
+import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
